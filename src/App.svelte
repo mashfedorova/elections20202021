0cc803bbe2794448a53afc50e07b5baea90be331
FEDOROVA Maria <marie.fedorova@gmail.com>
2021-08-31 16:55:43 +0200

    load dataset

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,19 @@
 <script>
+import { csv } from 'd3';
+import { onMount } from 'svelte';
 
+let data = [];
+
+onMount(async () => {
+  const dataRaw = await csv('data/dataFinal.csv', d => {
+    return {
+    ...d,
+    position: +d.position
+    }
+  })
+  data = dataRaw;
+  console.log(data)
+});
 </script>
 
 
